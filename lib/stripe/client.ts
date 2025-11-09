a9ea5bb378f0dbb53797d0ec0cfb811f5405eb1a
Lucas Walsh <walsl035.318@gmail.com>
2025-11-09 19:02:38 +0000

    fix params type

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2023-10-16' as any,
   typescript: true,
 });
 
